b10c9b9bd179758cc4e204aa93514d71aa1671e2
Parth Verma <v.parth98@gmail.com>
2018-08-10 20:53:08 +0530

    Added login auth logic and endpoint

--- models/user.js
+++ models/user.js
@@ -4,10 +4,17 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     name: {type:String,required:true},
-    username: {type:String,required:true},
-    password: {type:String,required:true},
+    username: {
+        type:String,
+        unique: true,
+        required: true,
+        trim: true
+    },
     updated: { type: Date, default: Date.now },
     date_of_birth: { type: Date, required: true}
 });
+
+UserSchema.plugin(require('mongoose-bcrypt'));
+
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var Users = require('../models/user');
+const {body, validationResult} = require('express-validator/check');
+const {sanitizeBody} = require('express-validator/filter');
 /*
 * GET home page.
 * Add home page here with links to login page
 */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
+router.post('/login',
+    // Validate that the name field is not empty.
+    body('username', 'Username required').isLength({min: 1}).trim(),
+
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('username').trim().escape(),
+
+    function (req, res, next) {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            // Error messages can be returned in an array using `errors.array()`.
+        }
+        else {
+            // Perform Auth Checks
+            Users.findOne({username:req.body.username},'password')
+                .exec(function (err, user) {
+                    user.verifyPassword(req.body.password,function (err, valid) {
+                        if (err) {
+                            console.log(err)
+                        } else if (valid) {
+                            // Call correct view here
+                            // Insert mongo ObjectID sessions as userID
+                            // insert middleware call for all routes that require logins
+                            console.log('Valid (callback)');
+                        } else {
+                            console.log('Invalid (callback)');
+                        }
+                    })
+                })
+        }
+    }
+);
 module.exports = router;
